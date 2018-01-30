23f320ad01bd6c2cf5e9a13141a4ad71cac2a9c1
Jesús Silva <hola@jesussilva.es>
2018-01-30 09:15:35 +0100

    Elevator upload

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,72 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.interval;
+    this.direction = 'up';
+    this.waitingList = [];
+    this.passengers = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.interval = setInterval(()=>{
+      this.floorUp();
+      this.update();
+    },250);
+  }
+
+  stop() {
+    this.interval = clearInterval();
+  }
+
+  update() {
+    return this.log();
+  }
+
+  _passengersEnter() {
+    if (this.waitingList.length != 0) {
+      for (let i = 0; i < this.waitingList.length ;i++){
+        this.passengers.push(this.waitingList[i]);
+        this.waitingList[i].splice(i,1);
+        console.log(`${this.waitingList[i].name} has enter the elevator`);
+        this.requests.push(this.waitingList[i].destinationFloor)
+      }
+    }
+  }
+
+  _passengersLeave() {
+
+  }
+
+  floorUp() {
+    if (this.floor < 10 && this.direction == "up"){
+      this.floor++;
+    }else{
+      this.direction = "down";
+      this.floorDown();
+    }
+  }
+
+  floorDown() {
+    if (this.floor > 0 && this.direction == "down"){
+      this.floor--;
+    }else {
+      this.direction = "up";
+      this.floorUp();
+    }
+  }
+
+  call(person) {
+    this.requests.push(person.originFloor);
+    this.waitingList.push(person);
+    console.log(this.waitingList)
+    console.log(this.requests)
+  }
+
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,12 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+const myElev = new Elevator();
+myElev.start();
+
+let pepe = new Person("pepe", 5, 10);
+let juan = new Person("juan", 9, 8);
+let manu = new Person("manu", 2, 6);
+myElev.call(pepe);
+myElev.call(juan);
+myElev.call(manu);
